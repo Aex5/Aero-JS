cc86f017532f9e6f981548323e336f3af0cb392b
Aex5 <ytfuga02@gmail.com>
2024-01-25 18:18:01 +0700

    add: server class

---
+++ core/server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+class Server {
+    constructor(router, middleware){
+        this.router = router;
+        this.middleware = middleware;
+    }
+
+    start(port){
+        const server = http.createServer(function (req, res){
+            // proses
+        })
+
+        server.listen(port, function () {
+            console.log('running on ' + port);
+        })
+    }
+}
+
+module.exports = Server;
